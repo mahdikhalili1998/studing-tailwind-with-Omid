5c606cda14ab689e398fb2d9f362a019ad7bb394
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-03-21 18:48:31 +0330

    file added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="font-farsiNum px-4 flex-col justify-normal gap-1 bg-slate-300 flex ">
+      <p>سلام بچه ها حالتون چ ره</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+      <p>1233434</p>
+    </div>
+  );
+}
+
+export default App;
